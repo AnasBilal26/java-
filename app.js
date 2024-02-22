ed569f2129176507db93b1b8e2a786ca3197026f
AnasBilal26 <anasbilalbhai@gmail.com>
2024-02-23 01:22:02 +0500

    one java and one html file

---
+++ app.js
@@ -0,0 +1,180 @@
+// 1st task
+
+// chp1
+
+// q1
+
+alert("hello world");
+
+// q2
+
+alert("hello world my name is anas");
+
+// q3
+
+alert("welcome to js land \nHappy coding");
+
+// q4
+alert("welcome to js land");
+alert("Happy coding!\nPrevent this page from creaing additional dialogs");
+
+// q5
+
+alert("Hello... i can run js through my web  browsers console");
+
+// q6
+
+alert("Yes im using alert in my new html and css file");
+
+// q7
+alert("a and c is working b and d is not working");
+
+// chp2
+
+// q1
+
+var username = "AnasBilal26";
+
+// q2
+var myname = "AnasBilal";
+document.write(myname);
+// q3
+var message = "Hello world";
+alert(message);
+
+// q4
+
+var name = "Anas Bilal";
+var age = 20;
+var qualification = "Web and app developer";
+
+alert(name);
+alert(age);
+alert(qualification);
+
+q5;
+
+var piz = "pizza\npizz\npiz\npi\np";
+alert(piz);
+
+// q6
+
+var email = "AnasBilalbhai@gmail.com";
+alert("My email address is " + email);
+
+q7;
+
+var bookandgive = "A samrter way to learn javascript ";
+alert("Im tryng to learn from the book " + bookandgive);
+
+q8;
+var content = "Yah! i can write content through javasceipt";
+document.write(content);
+
+q9;
+
+var sign = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+alert(sign);
+
+// chp3
+
+// q1
+
+var age = 20;
+alert("I am " + age + " years old");
+
+q2;
+
+var N = 20;
+
+alert("you have visited this " + N + " times");
+
+Q3;
+
+var birthyear = 2004;
+document.write(
+  "My birth year is" +
+    birthyear +
+    "<br> Data type of my declared variable is number"
+);
+
+q4;
+var visitorname = "AnasBilal";
+var producttitle = "www.xyz clothing.com";
+var quantity = "5 t shirt (s)";
+
+document.write(visitorname + " ordered " + quantity + " on " + producttitle);
+
+// 2ndtask
+
+chp1;
+q1;
+
+alert("Anas");
+alert("Bilal");
+alert("anasbilalbhai@gmail.com");
+alert("0317*****45");
+
+q2;
+q3;
+alert("Hello world");
+chp2;
+
+q1;
+
+var myName = "AnasBilal";
+alert(myName);
+
+q2;
+
+var email = "anasbilalbhai@gmai.com";
+
+alert(email);
+
+q3;
+
+var teamname = "football Team";
+
+alert("Our team is " + teamname);
+
+// q4
+
+var bestman = "Anas";
+alert(bestman);
+
+chp3;
+
+q1;
+
+var caseqty = 40;
+alert(caseqty);
+
+q2;
+
+var caseqty = 144;
+alert(caseqty);
+
+q3;
+var num = "9";
+var numAsint = parseInt(num);
+
+alert(numAsint);
+
+q4;
+
+var number1 = 10;
+var number2 = 20;
+var numberplus = number1 + number2;
+alert(numberplus);
+// q5
+var merchTotal = 100;
+var shippingCharge = 10;
+var orderTotal = merchTotal + shippingCharge;
+alert(orderTotal);
+
+q6;
+
+var myNumber = 9;
+myNumber = myNumber + 5;
+
+alert(myNumber);
